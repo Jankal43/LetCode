356bdb4ffc0894741fa7c0c97690d7356bd4412c
Jan <jankaletka43@gmail.com>
2024-08-15 17:34:44 +0200

    validsudoku solved

---
+++ isValidSudoku.js
@@ -0,0 +1,53 @@
+function isValidSudoku(board) {
+    var box = [];
+    for (var i = 0; i < 3; i++) {
+        box[i] = [];
+        for (var j = 0; j < 3; j++) {
+            box[i][j] = [];
+            for (var k = 0; k < 9; k++) {
+                box[i][j][k] = 0;
+            }
+        }
+    }
+    for (var i = 0; i < board.length; i++) {
+        var rows = {};
+        var cols = {};
+        for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j] != ".") {
+                var boxRow = Math.floor(i / 3);
+                var boxCol = Math.floor(j / 3);
+                if (box[boxRow][boxCol][board[i][j]]) {
+                    return false;
+                }
+                else {
+                    box[boxRow][boxCol][board[i][j]] = 1;
+                }
+                if (rows[board[i][j]]) {
+                    return false;
+                }
+                else {
+                    rows[board[i][j]] = 1;
+                }
+            }
+            if (board[j][i] != ".") {
+                if (cols[board[j][i]]) {
+                    return false;
+                }
+                else {
+                    cols[board[j][i]] = 1;
+                }
+            }
+        }
+    }
+    return true;
+}
+var board = [["1", "2", ".", ".", "3", ".", ".", ".", "."],
+    ["4", "1", ".", "5", ".", ".", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", ".", "3"],
+    ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+    [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", ".", ".", ".", ".", ".", "2", ".", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]];
+console.log(isValidSudoku(board));
