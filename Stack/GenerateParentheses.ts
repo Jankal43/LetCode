a103c16e1e8628e8b9359e88eaa179988cc70dae
Jan <jankaletka43@gmail.com>
2024-08-26 20:21:56 +0200

    new

---
+++ Stack/GenerateParentheses.ts
@@ -0,0 +1,28 @@
+function generateParenthesis(n: number): string[] {
+    const result: string[] = [];
+
+    function backtrack(current: string, open: number, close: number): void {
+
+        if (current.length === 2 * n) {
+            result.push(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(current + '(', open + 1, close);
+        }
+
+        if (close < open) {
+            backtrack(current + ')', open, close + 1);
+        }
+    }
+
+    backtrack('', 0, 0);
+
+    return result;
+}
+
+
+const n = 3;
+console.log(generateParenthesis(n));
+
